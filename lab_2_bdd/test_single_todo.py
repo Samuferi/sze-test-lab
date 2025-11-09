492a43138323045dc5acde47cbc3b7e3ffba0561
Samuferi <fecoo2005@gmail.com>
2025-11-09 16:05:50 +0100

    lab_2 megoldas

--- lab_2_bdd/test_single_todo.py
+++ lab_2_bdd/test_single_todo.py
@@ -22,3 +22,62 @@ def client():
 def response(client):
     """A fixture to store the response from an API call."""
     return {}
+
+
+# Given Steps
+@given('the API has a to-do with id 2 and task "Build a Flask API"')
+def setup_existing_todo():
+    """Set up the initial state with a known to-do item."""
+    # Ensure our 'todos' list is in a known state for this test
+    global todos
+    todos[:] = [
+        {"id": 1, "task": "Learn TDD", "done": False},
+        EXISTING_TODO,
+    ]
+
+@given('the API has a list of to-dos')
+def setup_any_todos():
+    """Set up the initial state with a generic list."""
+    global todos
+    todos[:] = [
+        {"id": 1, "task": "Learn TDD", "done": False},
+        {"id": 2, "task": "Build a Flask API", "done": True},
+        {"id": 3, "task": "Example TODO", "done": False},
+    ]
+
+@when(parsers.parse('the user requests the to-do with id {todo_id}'))
+def get_single_todo(client, response, todo_id):
+    """Make a GET request to the /todos/<id> endpoint."""
+    res = client.get(f'/todos/{todo_id}')
+    response['data'] = res.get_json()
+    response['status_code'] = res.status_code
+
+# Then Steps
+@then(parsers.parse('the response status code should be {status_code:d}'))
+def check_status_code(response, status_code):
+    """Check if the response status code is the expected one."""
+    assert response['status_code'] == status_code
+
+@then('the response should contain the details of the to-do with id 2')
+def check_response_for_existing_todo(response):
+    """Check the content of the response for a successful request."""
+    assert response['data'] == EXISTING_TODO
+
+@then('the response should contain a "not found" error message')
+def check_response_for_not_found(response):
+    """Check the content of the response for a 404 error."""
+    assert 'error' in response['data']
+    assert 'not found' in response['data']['error'].lower()
+
+@when(parsers.parse('the user deletes the to-do with id {todo_id}'))
+def delete_single_todo(client, response, todo_id):
+    """Send DELETE request."""
+    res = client.delete(f'/todos/{todo_id}')
+    response['data'] = res.get_json()
+    response['status_code'] = res.status_code
+
+@then('the response should indicate successful deletion')
+def check_successful_deletion(response):
+    """The API should confirm the deletion."""
+    assert 'message' in response['data']
+    assert response['data']['message'].lower() == "todo deleted"
